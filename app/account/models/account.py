2637ff96a87baf1d1f835a664fa55e52d8fe6e0d
honghuynhit <honghuynhit@gmail.com>
2022-04-15 16:57:06 +0700

    update

--- app/account/models/account.py
+++ app/account/models/account.py
@@ -1,7 +1,9 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class RefAccount(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user_id = models.CharField(
         max_length=24,
         blank=True,

5e60212f33c0800ba1fa22081e92b336406b0a97
Đinh Hồng Huỳnh <dhhuynh@hspace.biz>
2022-08-03 18:10:55 +0700

    Update

--- app/event/apps.py
+++ app/event/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from utils.register_to_registry_service import register_to_registry_service
 
 class EventConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'event'
+
+    def ready(self):
+        register_to_registry_service()

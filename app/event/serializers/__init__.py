b2dc701ac1c89f84df4b31a370527c6cfd6125b8
Đinh Hồng Huỳnh <dhhuynh@hspace.biz>
2022-08-03 15:07:57 +0700

    Update

--- app/event/serializers/__init__.py
+++ app/event/serializers/__init__.py
@@ -1,5 +1,5 @@
-from .event_serializer import EventSerializer, EventSerializerOut, EventSerializerOutShort
+from .event_serializer import EventSerializer, EventSerializerOut
 from .event_type_serializer import EventTypeSerializer, EventTypeSerializerOut
 from .formality_serializer import FormalitySerializer, FormalitySerializerOut
 from .privacy_serializer import PrivacySerializer, PrivacySerializerOut
-from .event_participant_serializer import EventParticipantOut, EventParticipantSerializer
+from .event_participant_serializer import EventParticipantOut, EventParticipantSerializer, EventSerializerOutShort

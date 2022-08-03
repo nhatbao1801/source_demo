6eaf9f8c38a7a5d2c7f0ec3c728df188f5530833
Đinh Hồng Huỳnh <dhhuynh@hspace.biz>
2022-08-03 18:10:17 +0700

    Update

---
+++ app/utils/register_to_registry_service.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+from eventcenter.extra_settings.config import Config
+
+
+configuration = Config()
+
+def register_to_registry_service():
+    url = f"http://{configuration.registry_host}:{configuration.registry_port}/v1/agent/service/register"
+    data = {
+        "Name": "event_service",
+        "Tags": ["event_service"],
+        "Address": configuration.address,
+        'Port': int(configuration.port),
+        "Check": {
+            "http": f"http://{configuration.address}:{configuration.port}/common/health",
+            "interval": "10s"
+        }
+    }
+    print("Service registration parameters: ", data)
+    res = {"message": "registering"}
+    res = requests.put(url, data=json.dumps(data))
+    return res.text

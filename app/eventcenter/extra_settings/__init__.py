bdebd46893824c9fe4e039c4276062a4365de5f0
Đinh Hồng Huỳnh <dhhuynh@hspace.biz>
2022-08-03 18:01:52 +0700

    Update

--- app/eventcenter/extra_settings/__init__.py
+++ app/eventcenter/extra_settings/__init__.py
@@ -1,3 +1,4 @@
 from .drf_yasg import SWAGGER_SETTINGS, REDOC_SETTINGS
 # from .elasticsearch_dsl import ELASTICSEARCH_HOST, ELASTICSEARCH_DSL
-from .s3_storage import PublicMediaStorage, PrivateMediaStorage
+# from .s3_storage import PublicMediaStorage, PrivateMediaStorage
+from .config import Config

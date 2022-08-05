5b72df0309ed441acd0d6671e2ba6b93c8b0ecc3
Đinh Hồng Huỳnh <dhhuynh@hspace.biz>
2022-08-05 11:51:57 +0700

    Update

--- app/event/serializers/event_serializer.py
+++ app/event/serializers/event_serializer.py
@@ -35,7 +35,7 @@ class EventSerializerOut(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ['id','is_owner', 'is_joined', 'owner_info', 'name', 'cover', 'venue', 'tagline', 'description', 'short_description', 'from_date', 'to_date', 'users_interested_in_info', 'privacy_info', 'co_host_info', 'formality_info', 'event_type_info', 'event_participant_info', 'business_level_code', 'link_online', 'out_date']
+        fields = ['id','is_owner', 'is_joined', 'owner_info', 'name', 'cover', 'venue', 'tagline', 'description', 'short_description', 'from_date', 'to_date', 'users_interested_in_info', 'privacy_info', 'co_host_info', 'formality_info', 'event_type_info', 'event_participant_info', 'business_level_code', 'link_online', 'is_form_submited', 'out_date']
 
 
     def check_form_submited(self, inst):
